9531a624ad30d7f13722f9c2cb2e92ccdf69d171
Alquipo <alquiponeto@outlook.com.br>
2021-02-15 19:27:53 -0300

    fix: storybooks

--- src/components/PokemonInformation/stories.tsx
+++ src/components/PokemonInformation/stories.tsx
@@ -1,9 +1,18 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import PokemonInformation from '.'
+import PokemonInformation, { PokemonInformationProps } from '.'
 
 export default {
   title: 'PokemonInformation',
-  component: PokemonInformation
+  component: PokemonInformation,
+  args: {
+    id: 6,
+    height: 1.7,
+    wight: 90.5,
+    abilities: ['blaze', 'solar-power'],
+    types: ['Fire', 'Flying']
+  }
 } as Meta
 
-export const Default: Story = () => <PokemonInformation />
+export const Default: Story<PokemonInformationProps> = (args) => (
+  <PokemonInformation {...args} />
+)
--- src/components/PokemonStats/stories.tsx
+++ src/components/PokemonStats/stories.tsx
@@ -1,9 +1,40 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import PokemonStats from '.'
+import PokemonStats, { PokemonStatsProps } from '.'
 
 export default {
   title: 'PokemonStats',
-  component: PokemonStats
+  component: PokemonStats,
+  args: {
+    types: ['Fire', 'Flying'],
+    stats: [
+      {
+        baseStat: 100,
+        name: 'hp'
+      },
+      {
+        baseStat: 110,
+        name: 'attack'
+      },
+      {
+        baseStat: 80,
+        name: 'defense'
+      },
+      {
+        baseStat: 154,
+        name: 'Sp.attack'
+      },
+      {
+        baseStat: 90,
+        name: 'Sp.defense'
+      },
+      {
+        baseStat: 130,
+        name: 'speed'
+      }
+    ]
+  }
 } as Meta
 
-export const Default: Story = () => <PokemonStats />
+export const Default: Story<PokemonStatsProps> = (args) => (
+  <PokemonStats {...args} />
+)
